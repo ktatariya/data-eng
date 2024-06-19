4f2cf12c0a80722c1c26423cabf80869accab387
ktatariya <164814714+ktatariya@users.noreply.github.com>
2024-06-18 22:45:58 -0400

    adding more sources and files

--- dags/Batch_1.py
+++ dags/Batch_1.py
@@ -11,6 +11,10 @@ import batch_1.tasks.python.snowflake_meta as py3
 import batch_1.tasks.python.snowflake_staging as py4
 import batch_1.tasks.python.snowflake_reporting as py5
 
+
+config = Variable.get("CONFIG", deserialize_json=True)
+sources = config['SOURCES']
+
 # Define default arguments for the DAG
 default_args = {
     'owner': 'airflow',
@@ -31,41 +35,47 @@ dag = DAG('simple_batch_processing',
 start_task = DummyOperator(task_id='start', dag=dag)
 end_task = DummyOperator(task_id='end', dag=dag)
 
-task_a = PythonOperator(
-    task_id='raw_s3',
-    python_callable=py0.raw_s3,
-    dag=dag,
-)
-
-task_b = PythonOperator(
-    task_id='staging_s3',
-    python_callable=py1.staging_s3,
-    dag=dag,
-)
-
-task_c = PythonOperator(
-    task_id='snowflake_raw',
-    python_callable=py2.snowflake_raw,
-    dag=dag,
-)
-
-task_d = PythonOperator(
-    task_id='snowflake_meta',
-    python_callable=py3.snowflake_meta,
-    dag=dag,
-)
-
-task_e = PythonOperator(
-    task_id='snowflake_staging',
-    python_callable=py4.snowflake_staging,
-    dag=dag,
-)
-
-task_f = PythonOperator(
-    task_id='snowflake_reporting',
-    python_callable=py5.snowflake_reporting,
-    dag=dag,
-)
-
+for client, properties in sources.items():
+    vendor_name = DummyOperator(task_id=f'{client}',retries=3, dag=dag)
+    start_task >> vendor_name
+    for file_object, file_properties in properties ['files'].items():
+        task_a = PythonOperator(
+            task_id=f'{file_object}_raw_s3',
+            python_callable=py0.raw_s3,
+            dag=dag,
+        )
+        vendor_name >> task_a
+        task_b = PythonOperator(
+            task_id=f'{file_object}_staging_s3',
+            python_callable=py1.staging_s3,
+            dag=dag,
+        )
+        task_a >> task_b
+        task_c = PythonOperator(
+            task_id=f'{file_object}_snowflake_raw',
+            python_callable=py2.snowflake_raw,
+            dag=dag,
+        )
+        task_b >> task_c
+        task_d = PythonOperator(
+            task_id=f'{file_object}_snowflake_meta',
+            python_callable=py3.snowflake_meta,
+            dag=dag,
+        )
+        task_c >> task_d
+        task_e = PythonOperator(
+            task_id=f'{file_object}_snowflake_staging',
+            python_callable=py4.snowflake_staging,
+            dag=dag,
+        )
+        task_d >> task_e
+        task_f = PythonOperator(
+            task_id=f'{file_object}_snowflake_reporting',
+            python_callable=py5.snowflake_reporting,
+            dag=dag,
+        )
+        task_e >> task_f
+        
+        task_f >> end_task
 # Define task dependencies
-start_task >> task_a >> task_b >> task_c >> task_d >> task_e >> task_f >> end_task
+
